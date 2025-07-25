71e7b6c368dc197710d90acfd19e3dc2f14df7ea
Josieljcc <josiel.jcc@hotmail.com>
2025-07-25 16:23:25 -0300

    chore: change api port

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ func main() {
 	mux.HandleFunc("/book/", BookHandler)
 	// Handlers serão adicionados aqui
 
-	log.Println("Servidor iniciado na porta 8080")
-	err := http.ListenAndServe(":8080", mux)
+	log.Println("Servidor iniciado na porta 8081")
+	err := http.ListenAndServe(":8081", mux)
 	if err != nil {
 		log.Fatalf("Erro ao iniciar o servidor: %v", err)
 	}
